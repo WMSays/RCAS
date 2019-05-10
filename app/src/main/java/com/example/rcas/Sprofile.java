e8677a3c7dc4765541d0744b47471e40c2f5b275
moheera <moheeranaz63@gmail.com>
2019-05-10 22:43:51 +0500

    Sprofilecheck

--- app/src/main/java/com/example/rcas/Sprofile.java
+++ app/src/main/java/com/example/rcas/Sprofile.java
@@ -3,11 +3,11 @@ package com.example.rcas;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class S_profile extends AppCompatActivity {
+public class Sprofile extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_s_profile);
+        setContentView(R.layout.activity_sprofile);
     }
 }
